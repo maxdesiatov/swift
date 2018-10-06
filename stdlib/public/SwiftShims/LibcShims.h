b04bc6801b30814c218cfc49868910c895c95649
Max Desiatov <max@desiatov.com>
2018-10-06 17:16:51 +0100

    Added missing defined(__wasm__) guard in LibcShims

--- stdlib/public/SwiftShims/LibcShims.h
+++ stdlib/public/SwiftShims/LibcShims.h
@@ -99,7 +99,7 @@ static inline __swift_size_t _swift_stdlib_malloc_size(const void *ptr) {
   return malloc_size(ptr);
 }
 #elif defined(__linux__) || defined(__CYGWIN__) || defined(__ANDROID__) \
-   || defined(__HAIKU__) || defined(__FreeBSD__)
+   || defined(__HAIKU__) || defined(__FreeBSD__) || defined(__wasm__)
 static inline __swift_size_t _swift_stdlib_malloc_size(const void *ptr) {
 #if defined(__ANDROID__)
 #if __ANDROID_API__ >= 17
